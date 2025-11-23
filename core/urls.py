038fb2fc1682eb552d7c47bc9ff14ab28646c119
S1272250610 <1272250610@mitwpu.edu.in>
2025-11-23 16:18:29 +0530

    fixed logging in

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib import admin
 from django.urls import path, include
 from accounts import views as account_views
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
    
