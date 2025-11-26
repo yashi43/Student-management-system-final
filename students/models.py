7e41cef965b9cf3fda362775bba4187dee967603
S1227250610 <S1227250610@mitwpu.edu.in>
2025-11-26 12:04:34 +0530

    fixed student model

--- students/models.py
+++ students/models.py
@@ -4,7 +4,6 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     roll_number = models.CharField(max_length=20)
-    address = models.TextField()
 
     def __str__(self):
         return self.name
